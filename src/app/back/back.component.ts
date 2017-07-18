69fc90e6327dddcb7f3690b85877ac498e0c6e15
e_ntyo <entyo.contact@gmail.com>
2017-07-18 16:20:42 +0900

    :pencil2: とりあえずコンポーネントに全部突っ込んだ

--- src/app/back/back.component.ts
+++ src/app/back/back.component.ts
@@ -18,40 +18,37 @@ export class BackComponent {
   @ViewChild('rendererContainer') rendererContainer: ElementRef;
 
   constructor() {}
+  scene: THREE.Scene;
+  camera: THREE.Camera;
+  renderer: THREE.WebGLRenderer;
+  textObjects: THREE.Mesh[] = [];
 
   ngAfterViewInit() {
-    let scene = new THREE.Scene()
+    this.scene = new THREE.Scene();
 
-    let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
-    let renderer = new THREE.WebGLRenderer()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    this.rendererContainer.nativeElement.appendChild(renderer.domElement)
+    this.renderer = new THREE.WebGLRenderer()
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    this.rendererContainer.nativeElement.appendChild(this.renderer.domElement)
 
-    let controls = new OrbitControls(camera); // Add orbit controll function
+    let controls = new OrbitControls(this.camera); // Add orbit controll function
 
     let axis = new THREE.AxisHelper(10)
-    scene.add(axis)
+    this.scene.add(axis)
 
     let light = new THREE.DirectionalLight(0xffffff, 1.0)
     light.position.set(100, 100, 100)
-    scene.add(light)
+    this.scene.add(light)
 
     let light2 = new THREE.DirectionalLight(0xffffff, 1.0)
     light2.position.set(-100, 100, -100)
-    scene.add(light2)
+    this.scene.add(light2)
 
     let material = new THREE.MeshBasicMaterial({
       color: 0xFFFFFF,
       wireframe: false
     })
-
-    const removeFromScene = (mesh: THREE.Mesh) => {
-      scene.remove(mesh);
-      mesh.geometry.dispose();
-      mesh.material.dispose();
-      mesh = undefined;
-    }
 
     // Add text
     let font = new THREE.FontLoader().parse(fontJSON);
@@ -65,9 +62,8 @@ export class BackComponent {
     recognition.maxAlternatives = 1;
     recognition.start();
 
-    let textObjects: THREE.Mesh[] = [];
     recognition.onresult = evt => {
-      console.log(textObjects);
+      console.log(this.textObjects);
       if (!evt.results) {
         return;
       }
@@ -82,9 +78,8 @@ export class BackComponent {
           bevelEnabled: false
         });
         let textObject = new THREE.Mesh(textGeometry, material);
-        textObject.position.set(window.innerWidth / 2, Math.random() * 200, 0);
-        textObjects.push(textObject);
-        scene.add(textObject);
+        this.textObjects.push(textObject);
+        this.scene.add(textObject);
       }
     }
     recognition.onerror = evt => {
@@ -92,137 +87,43 @@ export class BackComponent {
     }
 
     // Set camera position
-    camera.position.x = 0
-    camera.position.y = 0
-    camera.position.z = 300
+    this.camera.position.x = 0
+    this.camera.position.y = 0
+    this.camera.position.z = 300
 
-    camera.lookAt(scene.position)
+    this.camera.lookAt(this.scene.position)
 
-    const render = (): void => {
-      let timer = 0.002 * Date.now()
-      renderer.render(scene, camera)
-    }
-
-    const animate = (): void => {
-      textObjects.forEach((txt, i) => {
-        txt.position.x -= 4;
-        const bbox = new THREE.Box3().setFromObject(txt);
-        if (txt.position.x < -(window.innerWidth / 2 + (bbox.max.x - bbox.min.x))) {
-          removeFromScene(txt);
-          textObjects[i] = undefined;
-        }
-      });
-      // TODO: もっといいやり方を見つける
-      textObjects = textObjects.filter(txt => {
-        return txt;
-      });
-      requestAnimationFrame(animate)
-      render()
-    }
-
-    animate()
+    // animation start
+    this.animate();
   }
 
-  // ngOnInit() {
-  //   let scene = new THREE.Scene()
+  render() {
+    let timer = 0.002 * Date.now()
+    this.renderer.render(this.scene, this.camera)
+  }
 
-  //   let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+  removeFromScene(mesh: THREE.Mesh) {
+      this.scene.remove(mesh);
+      mesh.geometry.dispose();
+      mesh.material.dispose();
+      mesh = undefined;
+  }
 
-  //   let renderer = new THREE.WebGLRenderer()
-  //   renderer.setSize(window.innerWidth, window.innerHeight)
-  //   document.body.appendChild(renderer.domElement)
-
-  //   let controls = new OrbitControls(camera); // Add orbit controll function
-
-  //   let axis = new THREE.AxisHelper(10)
-  //   scene.add(axis)
-
-  //   let light = new THREE.DirectionalLight(0xffffff, 1.0)
-  //   light.position.set(100, 100, 100)
-  //   scene.add(light)
-
-  //   let light2 = new THREE.DirectionalLight(0xffffff, 1.0)
-  //   light2.position.set(-100, 100, -100)
-  //   scene.add(light2)
-
-  //   let material = new THREE.MeshBasicMaterial({
-  //     color: 0xFFFFFF,
-  //     wireframe: false
-  //   })
-
-  //   const removeFromScene = (mesh: THREE.Mesh) => {
-  //     scene.remove(mesh);
-  //     mesh.geometry.dispose();
-  //     mesh.material.dispose();
-  //     mesh = undefined;
-  //   }
-
-  //   // Add text
-  //   let font = new THREE.FontLoader().parse(fontJSON);
-  //   const { webkitSpeechRecognition }: IWindow = <IWindow>window;
-  //   const recognition = new webkitSpeechRecognition();
-  //   recognition.continuous = true;
-  //   recognition.lang = 'ja';
-  //   recognition.interimResults = true;
-  //   recognition.maxAlternatives = 1;
-  //   recognition.start();
-
-  //   let textObjects: THREE.Mesh[] = [];
-  //   recognition.onresult = evt => {
-  //     console.log(textObjects);
-  //     if (!evt.results) {
-  //       return;
-  //     }
-  //     const result = evt.results[evt.resultIndex];
-  //     if (result.isFinal && result[0].confidence > 0.3) {
-  //       // 取得したフォントを、TextGeometryのパラメータに渡す。
-  //       let textGeometry = new THREE.TextGeometry(result[0].transcript, {
-  //         font: font,
-  //         size: 10,
-  //         height: 5,
-  //         curveSegments: 12,
-  //         bevelEnabled: false
-  //       });
-  //       let textObject = new THREE.Mesh(textGeometry, material);
-  //       textObject.position.set(window.innerWidth / 2, Math.random() * 200, 0);
-  //       textObjects.push(textObject);
-  //       scene.add(textObject);
-  //     }
-  //   }
-  //   recognition.onerror = evt => {
-  //     console.log(evt);
-  //   }
-
-  //   // Set camera position
-  //   camera.position.x = 0
-  //   camera.position.y = 0
-  //   camera.position.z = 300
-
-  //   camera.lookAt(scene.position)
-
-  //   const render = (): void => {
-  //     let timer = 0.002 * Date.now()
-  //     renderer.render(scene, camera)
-  //   }
-
-  //   const animate = (): void => {
-  //     textObjects.forEach((txt, i) => {
-  //       txt.position.x -= 4;
-  //       const bbox = new THREE.Box3().setFromObject(txt);
-  //       if (txt.position.x < -(window.innerWidth / 2 + (bbox.max.x - bbox.min.x))) {
-  //         removeFromScene(txt);
-  //         textObjects[i] = undefined;
-  //       }
-  //     });
-  //     // TODO: もっといいやり方を見つける
-  //     textObjects = textObjects.filter(txt => {
-  //       return txt;
-  //     });
-  //     requestAnimationFrame(animate)
-  //     render()
-  //   }
-
-  //   animate()
-  // }
+  animate() {
+   this.textObjects.forEach((txt, i) => {
+      txt.position.x -= 4;
+      const bbox = new THREE.Box3().setFromObject(txt);
+      if (txt.position.x < -(window.innerWidth / 2 + (bbox.max.x - bbox.min.x))) {
+        this.removeFromScene(txt);
+        this.textObjects[i] = undefined;
+      }
+    });
+    // TODO: もっといいやり方を見つける
+    this.textObjects = this.textObjects.filter(txt => {
+      return txt;
+    });
+    requestAnimationFrame(this.animate.bind(this));
+    this.render()
+  }
 
 }
